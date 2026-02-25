42d2768c4422678eb82ffc8549c4b81f527e3f82
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-02-25 12:26:14 +0100

    shared, DTOs folder + DTOs

---
+++ CyberQuiz.Shared/DTOs/CategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuiz.Shared.DTOs
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CompletedSubCategories { get; set; }
+        public int TotalSubCategories { get; set; }
+
+    }
+}
---
+++ CyberQuiz.Shared/DTOs/SubCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuiz.Shared.DTOs
+{
+    public class SubCategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsLocked { get; set; }
+        public int QuestionCount { get; set; }
+    }
+}

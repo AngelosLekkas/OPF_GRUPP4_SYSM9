e5fa941c569d2660072bfac24b11eca77a2d0fc1
AngelosLekkas <lekkas.angelos@gmail.com>
2026-02-25 22:00:40 +0100

    Add SubmitAnswerRequestDto & SubmitAnswerResponseDto

---
+++ CyberQuiz.Shared/DTOs/SubmitAnswerRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuiz.Shared.DTOs
+{
+    //Request: UI/API -> BLL när User svarar på en fråga
+    public class SubmitAnswerRequestDto
+    {
+        public int QuestionId { get; set; }
+        public int AnswerOptionId { get; set; }
+    }
+}
---
+++ CyberQuiz.Shared/DTOs/SubmitAnswerResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Text;
+
+namespace CyberQuiz.Shared.DTOs
+{
+    //Response: skickas tillbaka till UI efter att svaret sparats för att visa rätt/fel + uppdaterad progression
+    public class SubmitAnswerResponseDto
+    {
+        public int QuestionId { get; set; }
+        public int AnswerOptionId { get; set; }
+        public bool IsCorrect { get; set; }
+        public int CorrectAnswerOptionId { get; set; }
+        public double SubCategoryScorePercent { get; set; }
+        public bool SubCategoryCompleted { get; set; }
+    }
+}

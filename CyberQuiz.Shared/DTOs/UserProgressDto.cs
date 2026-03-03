5493015b64ab114e1ba86086de41a1e5229072b1
AngelosLekkas <lekkas.angelos@gmail.com>
2026-03-03 13:34:03 +0100

    Added GetUserProgressAsync to IQuizService + Added UserProgressDto to use for showing General User Progress

---
+++ CyberQuiz.Shared/DTOs/UserProgressDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuiz.Shared.DTOs
+{
+    //För att visa generell överblick av progress på "profile"
+    public class UserProgressDto
+    {
+        public int TotalSubCategories { get; set; }
+        public int CompletedSubCategories { get; set; }
+        public double OverallPercent { get; set; }
+    }
+}
